31e7e8bb6c0725621d0fea22621d2210c0105619
erik <erik@q32.com>
2020-07-03 14:42:28 -0400

    cli

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## [1.0.2]:
+ ### Fixed
+   - cli runnable
+
 ## [1.0.1]:
  ### Package
    - specify files
--- cli.js
+++ cli.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const CoverDiff = require('./cover-diff')
 const main = new CoverDiff()
 main.run()
